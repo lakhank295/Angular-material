7e350fa4c2db49d71f8dd880ba3c4628f72f5345
Lakhan Khandelwal <lakhan.khandelwal@neosofttech.com>
2020-08-13 17:45:11 +0530

    env added

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -9,7 +9,7 @@ export class CommonService {
   constructor() { }
 
   getTableData() {
-    return of([
+    return of([ 
       {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
       {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
       {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
